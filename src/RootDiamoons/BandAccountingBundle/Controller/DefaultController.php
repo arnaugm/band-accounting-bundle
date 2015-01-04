47e28b9697039819ac7f3759bd9211a577882a3c
Arnau González <arnau.gonzalez@brandcrumb.com>
2015-01-04 18:12:05 +0100

    bundle created

---
+++ src/RootDiamoons/BandAccountingBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace RootDiamoons\BandAccountingBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('RootDiamoonsBandAccountingBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/RootDiamoons/BandAccountingBundle/RootDiamoonsBandAccountingBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace RootDiamoons\BandAccountingBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class RootDiamoonsBandAccountingBundle extends Bundle
+{
+}
