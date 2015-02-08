d15070823c4c3357feb9f14273ad2f3c1a2e0e84
Arnau González <arnau.gonzalez@brandcrumb.com>
2015-02-08 22:27:58 +0100

    add activity form

--- src/RootDiamoons/BandAccountingBundle/Controller/ActivityController.php
+++ src/RootDiamoons/BandAccountingBundle/Controller/ActivityController.php
@@ -6,6 +6,7 @@ use RootDiamoons\BandAccountingBundle\Entity\Activity;
 use RootDiamoons\BandAccountingBundle\Form\ActivityType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class ActivityController extends Controller
 {
@@ -24,6 +25,7 @@ class ActivityController extends Controller
     public function newAction()
     {
         $activity = new Activity();
+        $activity->setDateValue(new \DateTime());
         $form = $this->createForm(new ActivityType(), $activity);
 
         return $this->render('RootDiamoonsBandAccountingBundle:Activity:new.html.twig', array(
@@ -31,23 +33,21 @@ class ActivityController extends Controller
         ));
     }
 
-//    public function createAction()
-//    {
-//        $activity = new Activity();
-//        $form = $this->createForm(new ActivityType(), $activity);
-//
-//        $form->handleRequest($request);
-//
-//        $em = $this->getDoctrine()->getManager();
-//
-//        if ($form->isValid()) {
-//
-//            $em->persist($activity);
-//            $em->flush();
-//        }
-//
-//        return $this->render('BrandCrumbMediaBundle:Media:new.html.twig', array(
-//            'form' => $form->createView(),
-//        ));
-//    }
+    public function createAction(Request $request)
+    {
+        $activity = new Activity();
+        $form = $this->createForm(new ActivityType(), $activity);
+
+        $form->handleRequest($request);
+
+        $em = $this->getDoctrine()->getManager();
+
+        if ($form->isValid()) {
+
+            $em->persist($activity);
+            $em->flush();
+        }
+
+        return $this->redirect($this->generateUrl('root_diamoons_band_accounting_activity'));
+    }
 }
