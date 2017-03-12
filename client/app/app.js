4c1c3ccb23ccc6bf45a42b879c1f7ba9b535a80a
Arnau González <arnaugm@gmail.com>
2017-03-12 23:20:09 +0100

    clean debugging

--- client/app/app.js
+++ client/app/app.js
@@ -1,15 +1,5 @@
 'use strict';
 
-(function(orig) {
-    angular.modules = [];
-    angular.module = function() {
-        if (arguments.length > 1) {
-            angular.modules.push(arguments[0]);
-        }
-        return orig.apply(null, arguments);
-    }
-})(angular.module);
-
 angular.module('app', [
   'ngMaterial',
   'directives.activitiesList',
